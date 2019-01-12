d52002df1df1a84231a77f6a38a776cad99f5f83
HerbertSu <37626040+HerbertSu@users.noreply.github.com>
2019-01-12 11:28:53 -0800

    Add read file lines

--- server.js
+++ server.js
@@ -5,23 +5,26 @@ const fs = require('fs');
 const app = express();
 
 
-// app.get("https://api.govinfo.gov/packages/CREC-2018-07-10/mods?api_key=UPY9jQU6CVq80XXWXWoDZzQ7JTm29GsvdJzACahT", (req, res) => {
+// app.get("https://api.govinfo.gov/packages/CREC-2018-12-21/mods?api_key=UPY9jQU6CVq80XXWXWoDZzQ7JTm29GsvdJzACahT", (req, res) => {
 //     res.send("hello")
 // })
 
-let date = "2018-12-21"
+// let date = "2018-12-21"
 
-fetch(`https://api.govinfo.gov/packages/CREC-${date}/mods?api_key=DEMO_KEY`)
-    .then(res => res.text())
-    .then(fullCR => {
-        // let jsCR = parseString(fullCR, (err, result)=>{
-        //     console.log(result)
-        // })
-        fs.writeFile("./test/CREC-2018-12-21.txt", fullCR, err=>{
-            if(err){
-                console.log(err)
-            }
-        })
-    })
+// fetch(`https://api.govinfo.gov/packages/CREC-${date}/mods?api_key=DEMO_KEY`)
+//     .then(res => res.text())
+//     .then(fullCR => {
+//         // let jsCR = parseString(fullCR, (err, result)=>{
+//         //     console.log(result)
+//         // })
+//         fs.writeFile("./test/CREC-2018-12-21.txt", fullCR, err=>{
+//             if(err){
+//                 console.log(err)
+//             }
+//         })
+//     })
+
+let xmlCR = fs.readFileSync("./test/CREC-2018-12-21.txt");
+console.log(xmlCR.toString());
 
 app.listen(3000);
