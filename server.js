8c5e452a4128b08c4ea93f9ae996cad073fa2dec
HerbertSu <37626040+HerbertSu@users.noreply.github.com>
2019-02-12 19:51:19 -0800

    Connection established to PostgreSQL database.

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const app = express();
-const CREC = require("./scripts/CREC/CREC");
-const DailyDigest = require("./scripts/DailyDigest/DailyDigest");
-const {parseCRECForCongVotes} = require('./scripts/CREC/parseCREC');
+// const CREC = require("./scripts/CREC/CREC");
+// const DailyDigest = require("./scripts/DailyDigest/DailyDigest");
+// const {parseCRECForCongVotes} = require('./scripts/CREC/parseCREC');
+
+const knex = require('knex');
+
+const postgres = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'password',
+        database: 'polaigo_test'
+    }
+});
+
+
+postgres.select('column1').from('test')
+    .then(data => {
+        console.log(data)
+    })
 
 
 //TODO: Find the indices of relatedItems that pertain to the pages returned in passedMeasureList and
@@ -19,9 +37,9 @@ const {parseCRECForCongVotes} = require('./scripts/CREC/parseCREC');
             includes rollCall. If so, grab the accompanying <extension> object
     */
 
-let relatedItems = CREC.relatedItems;
+// let relatedItems = CREC.relatedItems;
 
-let votedMeasuresExtensionElements = parseCRECForCongVotes(relatedItems);
+// let votedMeasuresExtensionElements = parseCRECForCongVotes(relatedItems);
 
-console.log(votedMeasuresExtensionElements.hrVotedMeasuresObj.passedBills[0])
+// console.log(votedMeasuresExtensionElements.hrVotedMeasuresObj.passedBills[0])
 
